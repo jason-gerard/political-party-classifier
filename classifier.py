aace6bf7c6d1b7e290948d84869d40dcfddd7852
Jason Gerard <jasongerard321@gmail.com>
2021-03-07 01:57:53 -0500

    added model validation and testing

--- classifier.py
+++ classifier.py
@@ -1,48 +1,20 @@
-import string
-import pandas as pd
-import nltk
-from nltk.corpus import stopwords, wordnet
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
+from build_df import build_df
+from sklearn import feature_extraction, model_selection, pipeline, metrics, svm
 
-df = pd.read_csv('./raw_dataset/training_set.csv')
+df = build_df()
 
+X = df['text']
+y = df['party_num_label']
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
 
-def get_wordnet_pos(word):
-    tag = nltk.pos_tag([word])[0][1][0].upper()
-    tag_dict = {
-        "J": wordnet.ADJ,
-        "N": wordnet.NOUN,
-        "V": wordnet.VERB,
-        "R": wordnet.ADV
-    }
+model = pipeline.Pipeline([
+    ('tfidf', feature_extraction.text.TfidfVectorizer()),
+    ('clf', svm.LinearSVC())
+])
 
-    return tag_dict.get(tag, wordnet.NOUN)
+model.fit(X_train, y_train)
 
+y_predict = model.predict(X_test)
 
-def clean_text(text):
-    text = text.lower()
-    words = word_tokenize(text)
-
-    # remove punctuation
-    table = str.maketrans('', '', string.punctuation)
-    words = [word.translate(table) for word in words]
-
-    # remove non alphabetic tokens
-    words = [word for word in words if word.isalpha()]
-
-    # remove stop words
-    stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word not in stop_words]
-
-    # remove word stems with lemmatization
-    lemmatizer = WordNetLemmatizer()
-    words = [lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in words]
-
-    return ' '.join(words)
-
-
-df2 = df.copy()[:10]
-print(df2.head())
-df2['text'] = df2['text'].apply(clean_text)
-print(df2.head())
+accuracy = metrics.accuracy_score(y_predict, y_test)
+print(f'SVC accuracy: {accuracy}')
